4b2ae821181d43f856b5a12821e58bf741caaeec
hunoia <youvm@outlook.com>
2026-05-12 14:15:10 +0800

    perf: lazy initialize data stores

--- app/src/main/java/hunoia/sideleap/utils/DataStoreHolder.kt
+++ app/src/main/java/hunoia/sideleap/utils/DataStoreHolder.kt
@@ -17,43 +17,43 @@ import hunoia.sideleap.ktx.dataStore
  */
 object DataStoreHolder {
 
-    val initialSettings: DataStore<InitialSettings> = run {
+    val initialSettings: DataStore<InitialSettings> by lazy {
         val fileName = DataStoreFiles.INITIAL_SETTINGS
         val defValue = InitialSettings()
         App.getContext().dataStore(fileName, defValue)
     }
 
-    val advancedSettings: DataStore<AdvancedSettings> = run {
+    val advancedSettings: DataStore<AdvancedSettings> by lazy {
         val fileName = DataStoreFiles.ADVANCED_SETTINGS
         val defValue = AdvancedSettings()
         App.getContext().dataStore(fileName, defValue)
     }
 
-    val gestureSettings: DataStore<GestureSettings> = run {
+    val gestureSettings: DataStore<GestureSettings> by lazy {
         val fileName = DataStoreFiles.GESTURE_SETTINGS
         val defValue = GestureSettings()
         App.getContext().dataStore(fileName, defValue)
     }
 
-    val actionSettings: DataStore<ActionSettings> = run {
+    val actionSettings: DataStore<ActionSettings> by lazy {
         val fileName = DataStoreFiles.ACTION_SETTINGS
         val defValue = ActionSettings()
         App.getContext().dataStore(fileName, defValue)
     }
 
-    val bottomGestureButtons: DataStore<List<GestureButton>> = run {
+    val bottomGestureButtons: DataStore<List<GestureButton>> by lazy {
         val fileName = DataStoreFiles.BOTTOM_GESTURE_BUTTONS
         val defValue = GestureButton.BottomDefaults
         App.getContext().dataStore(fileName, defValue)
     }
 
-    val sideGestureButtons: DataStore<List<GestureButton>> = run {
+    val sideGestureButtons: DataStore<List<GestureButton>> by lazy {
         val fileName = DataStoreFiles.SIDE_GESTURE_BUTTONS
         val defValue = GestureButton.SideDefaults
         App.getContext().dataStore(fileName, defValue)
     }
 
-    val quickAppLauncherSettings: DataStore<QuickAppLauncherSettings> = run {
+    val quickAppLauncherSettings: DataStore<QuickAppLauncherSettings> by lazy {
         val fileName = DataStoreFiles.QUICK_APP_LAUNCHER
         val defValue = QuickAppLauncherSettings()
         App.getContext().dataStore(fileName, defValue)
